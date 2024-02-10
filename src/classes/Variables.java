d32df84952390656b5a1a105364c467d920f254f
littlesekii <littlesekii@gmail.com>
2024-02-10 19:43:48 -0300

    primitive types

--- src/classes/Variables.java
+++ src/classes/Variables.java
@@ -10,9 +10,21 @@ public class Variables {
     }
 
     public void primitiveTypes() {
-        byte byten = 127; // 1 Byte -128 to 128
-        short shortn = 32767; // 2 Bytes -32768 to 32767
-        int intn = 2147483647; // 4 Bytes -2147483648 to 2147483647
-        long longn = 9223372036854775807L; // 8 Bytes -9223372036854775808 to 9223372036854775807
+
+        //Integer numbers
+        byte smaller = 127;                   // 1 Byte -128 to 128
+        short small = 32767;               // 2 Bytes -32768 to 32767
+        int medium = 2147483647;              // 4 Bytes -2147483648 to 2147483647
+        long big = 9223372036854775807L;  // 8 Bytes -9223372036854775808 to 9223372036854775807
+
+        //true or false
+        boolean isBrazilian = true;         // 1 Bit
+
+        //character
+        char character = 'D';               // 2 Bytes
+
+        float simpleReal = 1234.56f;      // 4 bytes
+        double biggerReal = 12345678.90;  // 8 bytes
+
     }
 }
