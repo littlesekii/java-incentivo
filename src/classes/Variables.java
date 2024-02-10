c9e2191f37a76440500fdcf2d0ff84faf5032bef
littlesekii <littlesekii@gmail.com>
2024-02-10 19:24:26 -0300

    variables

---
+++ src/classes/Variables.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Variables {
+    public void variables() {
+        String name = "Davi Bacalhau";
+        int age = 21;
+        boolean useGlasses = true;
+        double height = 1.75f;
+    }
+}
