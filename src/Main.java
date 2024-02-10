a58555ac56e2da307d1c221cb3e9fa406ffb7449
littlesekii <littlesekii@gmail.com>
2024-02-10 19:55:49 -0300

    added method output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import classes.Variables;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println();
+
+        Variables var = new Variables();
+
+        var.variables();
+        System.out.println();
+
+        var.primitiveTypes();
     }
 }
