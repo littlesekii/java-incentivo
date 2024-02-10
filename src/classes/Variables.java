e943b042f545a2113e81e7bda90299df901dfb64
littlesekii <littlesekii@gmail.com>
2024-02-10 19:36:08 -0300

    variables

--- src/classes/Variables.java
+++ src/classes/Variables.java
@@ -1,10 +1,18 @@
 package classes;
 
+@SuppressWarnings("unused")
 public class Variables {
     public void variables() {
         String name = "Davi Bacalhau";
         int age = 21;
         boolean useGlasses = true;
         double height = 1.75f;
+    }
+
+    public void primitiveTypes() {
+        byte byten = 127; // 1 Byte -128 to 128
+        short shortn = 32767; // 2 Bytes -32768 to 32767
+        int intn = 2147483647; // 4 Bytes -2147483648 to 2147483647
+        long longn = 9223372036854775807L; // 8 Bytes -9223372036854775808 to 9223372036854775807
     }
 }
